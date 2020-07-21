638adfb66a1dc322b145091081f4423456a03452
Stanley Kanagaraj <stanimman@gmail.com>
2020-07-21 18:33:31 +0530

    Added Spatial Softmax

--- model/imm_model.py
+++ model/imm_model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torchsummary import summary
 
 
@@ -14,18 +15,37 @@ class ImageEncoder(nn.Module):
         return x 
 
 class PoseEncoder(nn.Module):
-    def __init__(self,num_filter,nmaps=1):
+    def __init__(self,num_filter,nmaps=1,map_sizes=None,gauss_mode='gaus',):
         super(PoseEncoder, self).__init__()
+        self.map_sizes = map_sizes
+        self.gauss_mode = gauss_mode
         self.layers = encoder_block(num_filter)
         self.mod_layers = nn.ModuleList(self.layers)
         fin_shape = self.layers[-1][0].weight.shape[0]
         self.convf = nn.Conv2d(fin_shape,nmaps,kernel_size=1,stride=1)
-        #self.convf = nn.conv2d()
     def forward(self,x):
         for layer in self.mod_layers:
             x = layer(x)
         x = self.convf(x)
-        return x 
+        gauss_y, gauss_y_prob = self.get_coord(x,2, x.shape[1])  # B,NMAP
+        gauss_x, gauss_x_prob = self.get_coord(x,1, x.shape[2])  # B,NMAP
+        gauss_mu = torch.stack([gauss_y, gauss_x], axis=2)
+        gauss_xy = []
+        for map_size in self.map_sizes:
+            gauss_xy_ = get_gaussian_maps(gauss_mu, [map_size, map_size],
+                                      1.0 / 10,
+                                      mode=self.gauss_mode)
+        gauss_xy.append(gauss_xy_)
+        return  gauss_mu,gauss_xy
+    def get_coord(self,x,other_axis, axis_size):
+        # get "x-y" coordinates:
+        g_c_prob = torch.mean(x, axis=other_axis)  # B,W,NMAP
+        g_c_prob = F.softmax(g_c_prob, dim=1)  # B,W,NMAP
+        coord_pt = torch.linspace(-1.0, 1.0, axis_size) # W
+        coord_pt = coord_pt.view([1, axis_size, 1])
+        g_c = torch.sum(g_c_prob * coord_pt, axis=1)
+        return g_c, g_c_prob
+
 
 
 def conv_block (ni,nf,kernal,stride=1,pad=(3,3),batch_norm=True,activation='ReLu'):
@@ -57,5 +77,51 @@ device = torch.device("cpu")
 #with torch.no_grad():
 #    y= Encoder(torch.zeros(3,128,128, device=device))
 #print(y.shape)
-print(PoseEncoder(32,10))
-summary(PoseEncoder(32,10), input_size=(3, 128, 128))
+def get_gaussian_maps(mu, shape_hw, inv_std, mode='gaus'):
+  
+    mu_y, mu_x = mu[:, :, 0:1], mu[:, :, 1:2]
+
+    y = torch.linspace(-1.0, 1.0, shape_hw[0])
+
+    x = torch.linspace(-1.0, 1.0, shape_hw[1])
+
+    if (mode in ['rot', 'flat']):
+        mu_y, mu_x = torch.unsqueeze(mu_y, -1), torch.unsqueeze(mu_x, -1)
+
+        y = y.reshape([1, 1, shape_hw[0], 1])
+        x = x.reshape([1, 1, 1, shape_hw[1]])
+
+        g_y = torch.square(y - mu_y)
+        g_x = torch.square(x - mu_x)
+        dist = (g_y + g_x) * inv_std**2
+
+        if mode == 'rot':
+            g_yx = torch.exp(-dist)
+        else:
+            g_yx = torch.exp(-torch.pow(dist + 1e-5, 0.25))
+
+    if mode == 'gaus':
+        y = y.reshape([1, 1, shape_hw[0]])
+        x = x.reshape([1, 1, shape_hw[1]])
+        g_y = torch.exp(-torch.sqrt(1e-4+torch.abs((mu_y-y)*inv_std)))
+        g_x = torch.exp(-torch.sqrt(1e-4+torch.abs((mu_x-x)*inv_std)))
+
+        g_y = torch.unsqueeze(g_y, dim=3)
+        g_x = torch.unsqueeze(g_x, dim=2)
+        g_yx = torch.matmul(g_y, g_x)  # [B, NMAPS, H, W]
+
+    else:
+        raise ValueError('Unknown mode: ' + str(mode))
+
+    g_yx = g_yx.permute([0, 2, 3, 1])
+    return g_yx
+
+#print(PoseEncoder(32,10,[128,128]))
+#summary(PoseEncoder(32,10,[128,128]), input_size=(3, 128, 128))
+#PoseEncoder(32,10,[128,128]
+#torch.randn((1,3, 5, 5))
+#in = 
+inpu = torch.randn((2,3, 128, 128))
+Net = ImageEncoder(32)
+out  = Net(inpu)
+out.shape

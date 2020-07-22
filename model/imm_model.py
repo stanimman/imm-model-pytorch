8e85c4b9d4e231eaecb689090513aaed2e2ea139
Stanley Kanagaraj <stanimman@gmail.com>
2020-07-22 09:47:53 +0530

    final_rough_model

--- model/imm_model.py
+++ model/imm_model.py
@@ -15,28 +15,30 @@ class ImageEncoder(nn.Module):
         return x 
 
 class PoseEncoder(nn.Module):
-    def __init__(self,num_filter,nmaps=1,map_sizes=None,gauss_mode='gaus',):
+    def __init__(self,num_filter,inv_std,nmaps=1,map_sizes=None,gauss_mode='gaus'):
         super(PoseEncoder, self).__init__()
         self.map_sizes = map_sizes
         self.gauss_mode = gauss_mode
         self.layers = encoder_block(num_filter)
         self.mod_layers = nn.ModuleList(self.layers)
+        self.inv_std = inv_std
         fin_shape = self.layers[-1][0].weight.shape[0]
         self.convf = nn.Conv2d(fin_shape,nmaps,kernel_size=1,stride=1)
     def forward(self,x):
         for layer in self.mod_layers:
             x = layer(x)
         x = self.convf(x)
+        x = x.permute([0,2,3,1])
         gauss_y, gauss_y_prob = self.get_coord(x,2, x.shape[1])  # B,NMAP
         gauss_x, gauss_x_prob = self.get_coord(x,1, x.shape[2])  # B,NMAP
         gauss_mu = torch.stack([gauss_y, gauss_x], axis=2)
-        gauss_xy = []
+        #gauss_xy = []
         for map_size in self.map_sizes:
             gauss_xy_ = get_gaussian_maps(gauss_mu, [map_size, map_size],
-                                      1.0 / 10,
-                                      mode=self.gauss_mode)
-        gauss_xy.append(gauss_xy_)
-        return  gauss_mu,gauss_xy
+                                      1.0 / self.inv_std,
+                                      mode=self.gauss_mode) 
+        #gauss_xy.append(gauss_xy_)
+        return  gauss_mu,gauss_xy_
     def get_coord(self,x,other_axis, axis_size):
         # get "x-y" coordinates:
         g_c_prob = torch.mean(x, axis=other_axis)  # B,W,NMAP
@@ -49,17 +51,18 @@ class PoseEncoder(nn.Module):
 
 
 def conv_block (ni,nf,kernal,stride=1,pad=(3,3),batch_norm=True,activation='ReLu'):
-    conv_block =  nn.Sequential(
-    nn.Conv2d(in_channels=ni,out_channels=nf,kernel_size=kernal,stride=stride,padding=pad),
-    nn.BatchNorm2d(nf),
-    nn.ReLU()
-    )
+    conv_block =  nn.Sequential()
+    conv_block.add_module('conv',nn.Conv2d(in_channels=ni,out_channels=nf,kernel_size=kernal,stride=stride,padding=pad))
+    if batch_norm == True:
+        conv_block.add_module('batch_norm',nn.BatchNorm2d(nf))
+    if activation == 'ReLu':
+        conv_block.add_module('activation',nn.ReLU())
     return conv_block
 
 def encoder_block(num_filter):
     filter = num_filter
     layers = []
-    conv1 = conv_block(ni=3,nf =filter,kernal=7,stride=1,pad=(3,3))
+    conv1 = conv_block(ni=3,nf =filter,kernal=7,stride=1,pad=(3,3)) # Need to remove hard coding
     conv2 = conv_block(ni=filter,nf=filter,kernal=3,stride=1,pad=(1,1))
     filter1 = filter*2
     conv3 = conv_block(ni=filter,nf=filter1,kernal=3,stride=2,pad=(1,1))
@@ -113,8 +116,55 @@ def get_gaussian_maps(mu, shape_hw, inv_std, mode='gaus'):
     else:
         raise ValueError('Unknown mode: ' + str(mode))
 
-    g_yx = g_yx.permute([0, 2, 3, 1])
+    #g_yx = g_yx.permute([0, 2, 3, 1])
     return g_yx
+
+def decoder_block(num_filter,map_size): # Need to check if we require map_size
+    filter = num_filter*8
+    layers = []
+    conv1 = conv_block(ni=258,nf =filter,kernal=3,stride=1,pad=(1,1)) ## Need to remove hard coding
+    conv2 = conv_block(ni=filter,nf=filter,kernal=3,stride=1,pad=(1,1))
+    filter1 = int(filter/2.0)
+    upsamp_1 = nn.Upsample(scale_factor=2,mode='bilinear')
+    conv3 = conv_block(ni=filter,nf=filter1,kernal=3,stride=1,pad=(1,1))
+    conv4 = conv_block(ni=filter1,nf=filter1,kernal=3,stride=1,pad=(1,1))
+    filter2 = int(filter1/2)
+    upsamp_2 = nn.Upsample(scale_factor=2,mode='bilinear')
+    conv5 = conv_block(ni=filter1,nf=filter2,kernal=3,stride=1,pad=(1,1))
+    conv6 = conv_block(ni=filter2,nf=filter2,kernal=3,stride=1,pad=(1,1))
+    filter3 = int(filter2/2)
+    upsamp_3 = nn.Upsample(scale_factor=2,mode='bilinear')
+    conv7 = conv_block(ni=filter2,nf=filter3,kernal=3,stride=1,pad=(1,1),batch_norm=False,activation=False)
+    conv8 = conv_block(ni=filter3,nf=filter3,kernal=3,stride=1,pad=(1,1),batch_norm=False,activation=False)
+    layers = [conv1,conv2,upsamp_1,conv3,conv4,upsamp_2,conv5,conv6,upsamp_3,conv7,conv8]
+    return layers
+
+class ImageDecoder(nn.Module):
+    def __init__(self,num_filter,final_channel_size):
+        super(ImageDecoder, self).__init__()
+        self.layers = decoder_block(32,16)
+        self.mod_layers = nn.ModuleList(self.layers)
+        fin_shape = self.layers[-1][0].weight.shape[0]
+        self.convf = nn.Conv2d(fin_shape,final_channel_size,kernel_size=1,stride=1)
+    def forward(self,x):
+        for layer in self.mod_layers:
+            x = layer(x)
+        x = self.convf(x)
+        return x 
+
+class lmm_model(nn.Module):
+  def __init__(self,num_filter,final_channel_size,inv_std,nmaps=1,map_sizes=None,gauss_mode='gaus'):
+    super(lmm_model, self).__init__()
+    self.image_encoder = ImageEncoder(num_filter)
+    self.pose_encoder = PoseEncoder(num_filter,inv_std,nmaps=nmaps, map_sizes=map_sizes, gauss_mode='gaus')
+    self.image_decoder = ImageDecoder(num_filter, final_channel_size)
+
+  def forward(self,im,fut_im):
+    enc_image = self.image_encoder(im)
+    cord_pts,gauss_heatmap = self.pose_encoder(fut_im)
+    embedding = torch.cat([enc_image,gauss_heatmap],dim=1)
+    gen_image = self.image_decoder(embedding)
+    return gen_image
 
 #print(PoseEncoder(32,10,[128,128]))
 #summary(PoseEncoder(32,10,[128,128]), input_size=(3, 128, 128))
@@ -123,5 +173,19 @@ def get_gaussian_maps(mu, shape_hw, inv_std, mode='gaus'):
 #in = 
 inpu = torch.randn((2,3, 128, 128))
 Net = ImageEncoder(32)
-out  = Net(inpu)
-out.shape
+enc_img  = Net(inpu)
+print(enc_img.shape)
+Net1 = PoseEncoder(32,10,2,[16,16])    
+co_ord,gauss_heat = Net1(inpu)
+Net2  = ImageDecoder(32,3)
+embedding = torch.cat([enc_img,gauss_heat],dim=1)
+g_m = Net2(embedding)
+print(g_m.shape)
+
+
+fin_model = lmm_model(num_filter=32,final_channel_size=3,inv_std=10,nmaps=2,map_sizes=[16],gauss_mode='gaus')
+img_in = torch.randn((2,3, 128, 128))
+fut_img_in = torch.randn((2,3, 128, 128))
+gen_img  = fin_model(img_in,fut_img_in)
+gen_img.shape
+print(gen_img.shape)
